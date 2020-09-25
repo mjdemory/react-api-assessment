8922ef9913adc171158787e4511379644ceb0d4d
Michael DeMory <mjdemory2891@gmail.com>
2020-09-25 15:33:20 -0400

    initial commit. shoes are able to display.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shoes:[],
+      
+    };
+  }
+
+componentDidMount() {
+  fetch("http://127.0.0.1:8000/api/shoe/")
+  .then(res => res.json())
+  .then(data => this.setState ({shoes: data}));
+}
+
+  render() {
+    return (
+    <div>
+      <ul>
+        {this.state.shoes.map(s => (
+          <div>
+          <ul>
+            <li>
+              <h3>Brand Name:</h3>{s.brand_name}
+              <h3>Manufacturer:</h3>{s.manufacturer}
+              <h3>Shoe Type:</h3>{s.shoe_type}
+              <h3>Shoe Color:</h3>{s.color}
+              <h3>Shoe Material:</h3>{s.material}
+              <h3>Shoe Size:</h3>{s.size},
+              <h3>Shoe Fasten Type:</h3>{s.fasten_type}
+            </li>
+          </ul>
+
+          </div>
+        ))}
+      </ul>
     </div>
+   
+      
   );
+  }
+  
 }
 
 export default App;
